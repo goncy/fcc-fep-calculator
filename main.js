638bdfefff4feb0951acd0a182e61ee877609c65
goncy <gonzalo.pozzo4@gmail.com>
2017-12-22 07:27:32 -0300

    Finished

--- main.js
+++ main.js
@@ -1,6 +1,47 @@
+const isNum = str => !isNaN(Number(str))
+
 window.app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue.js!'
+    error: '',
+    buffer: '',
+    result: '0'
+  },
+  methods: {
+    addToBuffer(char) {
+      this.error = ''
+ 
+      const lastBufferCharIsNum = isNum(this.lastBufferChar)
+      const charIsNum = isNum(char)
+
+      if (lastBufferCharIsNum || charIsNum) {
+        this.buffer += char
+      } else {
+        this.buffer = this.previousBuffer + char
+      }
+    },
+    processBuffer() {
+      try {
+        this.result = eval(this.result + this.buffer)
+        this.buffer = ''
+      } catch (e) {
+        this.error = `Can't calculate this`
+        this.buffer = ''
+        this.result = 0
+      }
+    },
+    clearBuffer() {
+      this.buffer = ''
+      this.result = '0'
+      this.error = ''
+    }
+  },
+  computed: {
+    lastBufferChar() {
+      return this.buffer.substr(this.buffer.length - 1)
+    },
+    previousBuffer() {
+      return this.buffer.substring(0, this.buffer.length - 1)
+    }
   }
 })
